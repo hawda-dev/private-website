d250d34a023e1e43d9d77f6c1dee0f37f90f2f16
hawda-dev <hadihawda566@gmail.com>
2025-03-22 22:20:05 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 function check(form) {
-    username = new Array("admin1", "admin2", "members", "hawda@hawda.store");
-    password = new Array("admin1", "admin2", "members", "hawda1234");
+    username = new Array("admin1", "admin2", "members", "hawda@hawda.store", "aquapapi@gmail.com");
+    password = new Array("admin1", "admin2", "members", "hawda1234", "Covid201920");
 
-	if(form.userid.value == username[0] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
+	if(form.userid.value == username[1] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
 		location.replace('#adminhome')/* The Administrator homepage */
 	}
-	else if(form.userid.value == username[2] && form.pswrd.value == password[2]) {
-		location.replace('members/home.html')/* The members homepage */
+	else if(form.userid.value == username[5] && form.pswrd.value == password[5]) {
+		location.replace('papi.html')/* The members homepage */
 	}
 		else if(form.userid.value == username[3] && form.pswrd.value == password[3]) {
 		location.replace('members/home.html')/* The members homepage */
