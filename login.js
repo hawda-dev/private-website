2b086565c9eed3e666e3a228ab20475778a9b012
hawda-dev <hadihawda566@gmail.com>
2025-03-23 23:55:47 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function check(form) {
-    username = new Array("admin1", "admin2", "members", "hawda@hawda.store", "aquapapi@gmail.com", "sjbdindbsofd@gmail.com");
-    password = new Array("admin1", "admin2", "members", "hawda1234", "Covid201920", "baconburgerdisaster");
+    username = new Array("admin1", "admin2", "members", "hawda@hawda.store", "aquapapi@gmail.com", "sjbdindbsofd@gmail.com", "xd3911820@gmail.com");
+    password = new Array("admin1", "admin2", "members", "hawda1234", "Covid201920", "baconburgerdisaster", "b2rfpIYFMVfE2Ny");
 
 	if(form.userid.value == username[1] && form.pswrd.value == password[0]||form.userid.value == username[1] && form.pswrd.value == password[1]) {
 		location.replace('#adminhome')/* The Administrator homepage */
@@ -13,6 +13,9 @@ function check(form) {
 	}
 		else if(form.userid.value == username[3] && form.pswrd.value == password[3]) {
 		location.replace('members/home.html')/* The members homepage */
+	}
+			else if(form.userid.value == username[6] && form.pswrd.value == password[6]) {
+		location.replace('lloydgarmadon43.html')/* The members homepage */
 	}
 	else if(form.userid.value == username[0]||form.userid.value == username[1]||form.userid.value == username[2]) {
 		alert("Incorrect Password")
